801a2be688323f1598524b432af3a444cafd5bfa
LeandroMariano2023 <153309185+LeandroMariano2023@users.noreply.github.com>
2024-04-22 17:55:15 -0300

    ,

---
+++ variaveis/script.js
@@ -0,0 +1,15 @@
+var nome = 'Andre';
+var idade =  '28';
+var  possuiFaculdade = true
+
+console.log(nome, idade, possuiFaculdade) 
+
+var preco  = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco)
+
+var sobrenome = 'Mariano', cidade = 'Recife'
+
+console.log(cidade)
