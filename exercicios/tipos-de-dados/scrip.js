2f7e8184a772b02f1eee7dd8f6dea4df087aac59
LeandroMariano2023 <153309185+LeandroMariano2023@users.noreply.github.com>
2024-04-25 18:16:02 -0300

    .

---
+++ exercicios/tipos-de-dados/scrip.js
@@ -0,0 +1,27 @@
+// Declare uma variável contendo uma string
+var nome = 'Leandro';
+console.log(typeof nome)
+
+// Declare uma variável contendo um número dentro de uma string
+var nome = 'Leandro';
+var idade = '33';
+var frase1 =`Meu nome é ${nome} tenho ${idade} anos`;
+console.log(frase1)
+
+// Declare uma variável com a sua idade
+var idade = 33;
+console.log(idade)
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas 
+var nome = 'Leandro'
+var sobrenome = 'Mariano'
+var nomeCompleto = nome + ' '+ sobrenome;
+console.log(nomeCompleto)
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase2 = `It's time`
+console.log(frase2)
+
+// Verifique o tipo da variável que contém o seu nome
+var nome = 'Leandro'
---
+++ tipos-de-dados/script.js
@@ -0,0 +1,14 @@
+var nome = 'Leandro';
+var idade = 33;
+var time = null;
+console.log(typeof time)
+
+var nome = 'Leandro'
+var sobrenome = 'Mariano'
+var nomeCompleto = nome + ' '+ sobrenome; 
+console.log(nomeCompleto)
+
+var gols = 1000;
+var frase1 = 'Romário fez ' + gols + ' gols';
+var frase2 = `Romário fez ${gols} gols`; // Utilizando Template S
+console.log(frase2)
