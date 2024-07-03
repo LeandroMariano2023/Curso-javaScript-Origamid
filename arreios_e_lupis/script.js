4b3401fef809609caed3db9c4155d8cada41716d
LeandroMariano2023 <153309185+LeandroMariano2023@users.noreply.github.com>
2024-07-03 18:32:22 -0300

    ,

--- arreios_e_lupis/script.js
+++ arreios_e_lupis/script.js
@@ -1,4 +1,4 @@
-var videoGames = ['PC','PS4','XBOX']
+var videoGames = ['PC', 'PS4', 'XBOX']
 console.log(videoGames[1])
 
 
@@ -6,13 +6,35 @@ console.log(videoGames[1])
 // FOR LOOP
 // possui 3 partes, inicio, condiçao, incremento 
 
-for(var numero = 0; numero <10; numero ++) {
+for (var numero = 0; numero < 4; numero++) {
     console.log(numero)
 }
 
 // While loop
 var i = 0;
-while (i<10){
+while (i <= 10) {
     console.log(i);
     i++;
 }
+
+
+var videoGames = ['PC', 'PS4', 'XBOX', '3DS'];
+
+for (var item = 0; item < 4; item++) {
+    console.log(videoGames[item]);
+    if (videoGames[item] === 'PS4') {
+        break; 
+    }
+}
+
+
+videoGames.forEach(function(item){
+    console.log(  item)
+})
+
+var frutas = ['banana', 'pera', 'maçã', 'abacaxi', 'uva']
+
+frutas.forEach(function(fruta, index){
+    frutas.pop();
+    console.log(fruta, index, frutas)
+});
---
+++ atribuiçao_e_ternario/script.js
@@ -0,0 +1,16 @@
+var numero = 20;
+var numero2 = 10;
+
+numero += numero2; //numero = numero + numero2
+console.log(numero)
+
+
+// operador ternario
+// condiçao ? , true, false
+
+var idade = 20;
+var naoPossuiDiabetes = true
+var podeBeber;
+podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'pode Beber' : 'nao pode Beber';
+
+console.log(podeBeber)
