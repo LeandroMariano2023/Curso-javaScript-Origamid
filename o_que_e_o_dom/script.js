d4a55caeaa7e4d2c99c1d236a53341f390549b3f
LeandroMariano2023 <153309185+LeandroMariano2023@users.noreply.github.com>
2024-07-04 13:14:34 -0300

    ,

--- o_que_e_o_dom/script.js
+++ o_que_e_o_dom/script.js
@@ -1,3 +1,10 @@
 // window.alert('BOM DIA!!');
 
 const h1Selecionado = document.querySelector('h1');
+const h1classes= h1Selecionado.classList;
+
+function callbackh1(){
+    console.log('Clicou em', h1Selecionado.innerText);
+}
+
+h1Selecionado.addEventListener('click', callbackh1)
