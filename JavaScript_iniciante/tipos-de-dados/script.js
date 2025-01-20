cf869d47b2a6330a2dab1b6528162e5730afbf4e
LeandroMariano2023 <153309185+LeandroMariano2023@users.noreply.github.com>
2025-01-20 16:07:35 -0300

    ,

--- JavaScript_iniciante/tipos-de-dados/script.js
+++ JavaScript_iniciante/tipos-de-dados/script.js
@@ -1,14 +1,16 @@
-var nome = 'Leandro';
-var idade = 33;
-var time = null;
-console.log(typeof time)
+var nome = "Leandro"; // string
+var idade = 33; // number
+var time = null; // null
+console.log(typeof  idade);
 
-var nome = 'Leandro'
-var sobrenome = 'Mariano'
-var nomeCompleto = nome + ' '+ sobrenome; 
-console.log(nomeCompleto)
+//concatenaçao de string
+var nome = "Leandro";
+var sobrenome = "Mariano";
+var nomeCompleto = nome + " " + sobrenome;
+console.log(nomeCompleto);
 
+// concatenaçao de string mais numeros
 var gols = 1000;
-var frase1 = 'Romário fez ' + gols + ' gols';
-var frase2 = `Romário fez ${gols} gols`; // Utilizando Template S
-console.log(frase2)
+var frase1 = "Romário fez " + gols + " gols";
+var frase2 = `Romário fez ${gols} gols`; // Utilizando Template String
+console.log(frase2);
---
+++ JavaScript_iniciante/tipos-de-dados/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+var nome = "Leandro"; // string
+var idade = 33; // number
+var time = null; // null
+console.log(typeof  idade);
