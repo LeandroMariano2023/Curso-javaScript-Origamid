28736ea6ffadf1d6c7bbe6ebdd775f2f0eca4092
LeandroMariano2023 <153309185+LeandroMariano2023@users.noreply.github.com>
2025-01-21 11:06:27 -0300

    ,

--- JavaScript_iniciante/numeros-e-operadores/script.js
+++ JavaScript_iniciante/numeros-e-operadores/script.js
@@ -13,39 +13,38 @@ console.log(soma, subtraçao, multiplicaçao, divisao, expoente, modulo);
 
 // operadores aritimeticos(String)
 
-var soma = '100' + 50;
-var subtraçao = '100' - 50;
-var multiplicaçao = '100' * '2';
-var divisao = 'comprei 10' / 2;
+var soma = "100" + 50;
+var subtraçao = "100" - 50;
+var multiplicaçao = "100" * "2";
+var divisao = "comprei 10" / 2;
 
-console.log(soma, subtraçao, multiplicaçao, divisao)
+console.log(soma, subtraçao, multiplicaçao, divisao);
 
 // NAN  = Not a Number
 var numero = 80;
-var unidade = 'kg';
+var unidade = "kg";
 var peso = numero + unidade;
 var pesoPorDois = peso / 2;
 
-console.log(numero, unidade, peso, pesoPorDois)
+console.log(numero, unidade, peso, pesoPorDois);
 
-// Ordem de operaçoes 
+// Ordem de operaçoes
 // começa por multiplicaçao e divisao, depois soma e subtraçao, prioridade pelo que estiver dentro de parenteses.
 
 var total1 = 20 + 5 * 2;
 var total2 = (20 + 5) * 2;
-var total3 = 20 / 2 * 5;
+var total3 = (20 / 2) * 5;
 var total4 = 10 + 10 * 2 + 20 / 2;
 
 console.log(total1, total2, total3, total4);
 
 // operadores aritimeticos (Unarios)
 // so tem um numero na equaçao
- 
 
 var incremento = 5;
 console.log(incremento++);
 console.log(incremento);
 
-var incremento2 = 5
-console.log (++incremento2)
-console.log  (incremento2);
+var incremento2 = 5;
+console.log(++incremento2);
+console.log(incremento2);
