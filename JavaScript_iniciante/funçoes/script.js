b9625c508f5e2f827b5f48a868484211a0d9d41f
LeandroMariano2023 <153309185+LeandroMariano2023@users.noreply.github.com>
2025-01-23 16:55:44 -0300

    ,

--- JavaScript_iniciante/funçoes/script.js
+++ JavaScript_iniciante/funçoes/script.js
@@ -68,18 +68,57 @@ function faltaVisitar(paisesVisitados) {
 }
 console.log(faltaVisitar(100));
 
-
 //funçoes conseguem acessar variaveis que vinheram antes e nao depois.
 var profissao = "DEV";
 function dados() {
   var nome = "Leandro";
   var idade = 34;
   function outrosDados() {
-    var endereço = 'casa';
+    var endereço = "casa";
     var idade = 35;
     return `${nome}, ${idade}, ${endereço}, ${profissao}`;
   }
   return outrosDados();
 }
 console.log(dados());
-console.log(outrosDados());
+// console.log(outrosDados());
+
+// // criar uma funçao com valor truthy
+// function isTruthy(dado) {
+//   return !!dado;
+// }
+
+// crie uma funçao de perimetro do quadrado
+
+function perimetroQuadrado(lado) {
+  return lado * 4;
+}
+console.log(perimetroQuadrado(4));
+
+// funçao de nome completo
+function nomeCompleto(nome, sobrenome) {
+  return nome + " " + sobrenome;
+}
+console.log(nomeCompleto("Leandro", "Mariano"));
+
+// funçao par
+function isPar(numero) {
+  var modulo = numero % 2;
+  if (modulo === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isPar(6));
+
+// funçao tipo de dado
+function tipoDeDado(dado) {
+  return typeof dado;
+}
+console.log(tipoDeDado([]));
+
+//funçao de eventlistener
+addEventListener("scroll", function () {
+  console.log("Leandro Mariano");
+});
--- JavaScript_iniciante/funçoes/tempCodeRunnerFile.js
+++ JavaScript_iniciante/funçoes/tempCodeRunnerFile.js
@@ -1,13 +1,5 @@
-var profissao = "DEV";
-function dados() {
-  var nome = "Leandro";
-  var idade = 34;
-  function outrosDados() {
-    var endereço = 'casa';
-    var idade = 35;
-    return `${nome}, ${idade}, ${endereço}, ${profissao}`;
-  }
-  return outrosDados();
+// funçao tipo de dado
+function tipoDeDado(dado) {
+  return typeof dado;
 }
-console.log(dados());
-console.log(outrosDados());
+console.log(tipoDeDado([]));
