d67d093879c2514a83be8fd17c04aa2ffa650cf8
LeandroMariano2023 <153309185+LeandroMariano2023@users.noreply.github.com>
2025-01-24 16:20:31 -0300

    ,

--- JavaScript_iniciante/objeto/script.js
+++ JavaScript_iniciante/objeto/script.js
@@ -1,23 +1,28 @@
 var pessoa = {
-    nome : 'Leandro',
-    idade: 33,
-}
+  nome: "Leandro",
+  idade: 33,
+};
 console.log(pessoa.idade);
 
-
 var quadrado = {
-    lados: 4,
-    area: function(lado){
-        return lado * lado;
-    },
-    perimetro  (lado) {
-        // console.log(this.lados);
-        return this.lados * lado;
-        
-    }
-}
+  lados: 4,
+  area: function (lado) {
+    return lado * lado;
+  },
+  perimetro(lado) {
+    // console.log(this.lados);
+    return this.lados * lado;
+  },
+};
 
 console.log(quadrado.area(5));
 console.log(quadrado.perimetro(5));
 
+// objeto sao reutilizaveis
+Math.PI;
+Math.random;
 
+var pi = Math.PI;
+console.log(pi);
+
+console.log(Math.random());
---
+++ JavaScript_iniciante/objeto/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+// objeto sao reutilizaveis
+Math.PI;
+Math.random;
+
+var pi = Math.PI;
+console.log(pi);
