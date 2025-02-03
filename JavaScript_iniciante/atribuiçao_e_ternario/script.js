c523287e0accfbb52d266a42e2d85e126f2d2bc0
LeandroMariano2023 <153309185+LeandroMariano2023@users.noreply.github.com>
2025-02-03 12:03:10 -0300

    ,

--- JavaScript_iniciante/atribuiçao_e_ternario/script.js
+++ JavaScript_iniciante/atribuiçao_e_ternario/script.js
@@ -1,16 +1,22 @@
+//atribuiçao
 var numero = 20;
 var numero2 = 10;
 
 numero += numero2; //numero = numero + numero2
-console.log(numero)
-
+console.log(numero);
 
 // operador ternario
 // condiçao ? , true, false
 
 var idade = 20;
-var naoPossuiDiabetes = true
+var naoPossuiDiabetes = true;
 var podeBeber;
-podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'pode Beber' : 'nao pode Beber';
+podeBeber = idade >= 18 && naoPossuiDiabetes ? "pode Beber" : "nao pode Beber";
 
-console.log(podeBeber)
+console.log(podeBeber);
+
+//If abreviado
+
+var possuiFaculdade = true;
+if (possuiFaculdade) console.log("Sim possui");
+else console.log("Nao possui");
---
+++ JavaScript_iniciante/atribuiçao_e_ternario/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+var possuiFaculdade = true;
+if (possuiFaculdade) console.log("Sim possui");
+else console.log("Nao possui");
