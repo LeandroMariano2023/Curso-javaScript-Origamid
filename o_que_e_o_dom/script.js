1da9a23db01c57c68d3271374b6113aa94817b92
LeandroMariano2023 <153309185+LeandroMariano2023@users.noreply.github.com>
2025-02-03 18:05:38 -0300

    ,

--- o_que_e_o_dom/script.js
+++ o_que_e_o_dom/script.js
@@ -1,10 +1,22 @@
 // window.alert('BOM DIA!!');
 
-const h1Selecionado = document.querySelector('h1');
-const h1classes= h1Selecionado.classList;
+console.log(window);
 
-function callbackh1(){
-    console.log('Clicou em', h1Selecionado.innerText);
+
+window.innerHeight; // retorna o tamanho do browser
+
+const href = window.location.href;
+console.log(href)
+if(href === 'http://127.0.0.1:5500/o_que_e_o_dom/index.html'){
+    console.log('E verdade')
 }
 
-h1Selecionado.addEventListener('click', callbackh1)
+
+const h1Selecionado = document.querySelector("h1");
+const h1classes = h1Selecionado.classList;
+
+function callbackh1() {
+  console.log("Clicou em", h1Selecionado.innerText);
+}
+
+h1Selecionado.addEventListener("click", callbackh1);
